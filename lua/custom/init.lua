0d514339f2276a0247ecb93fddf14909e5001c22
Daniel Wiratman <danielwiratman@gmail.com>
2023-05-15 18:02:10 +0700

    make relative number as default

--- lua/custom/init.lua
+++ lua/custom/init.lua
@@ -1,7 +1,12 @@
--- local autocmd = vim.api.nvim_create_autocmd
+local autocmd = vim.api.nvim_create_autocmd
 
 -- Auto resize panes when resizing nvim window
 -- autocmd("VimResized", {
 --   pattern = "*",
 --   command = "tabdo wincmd =",
 -- })
+-- set relative number everytime neovim starts
+autocmd("TermOpen", {
+	pattern = "*",
+	command = "setlocal number relativenumber",
+})
