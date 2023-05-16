56deb79ca6619f410b8442ac99e90bedeac8a5b7
Daniel Wiratman <danielwiratman@gmail.com>
2023-05-16 12:53:13 +0700

    change codeium keybindings

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -53,20 +53,6 @@ local plugins = {
   {
     "Exafunction/codeium.vim",
     event = "VeryLazy",
-    config = function()
-      vim.keymap.set("i", "<TAB>", function()
-        return vim.fn["codeium#Accept"]()
-      end, { expr = true })
-      vim.keymap.set("i", "<c-;>", function()
-        return vim.fn["codeium#CycleCompletions"](1)
-      end, { expr = true })
-      vim.keymap.set("i", "<c-,>", function()
-        return vim.fn["codeium#CycleCompletions"](-1)
-      end, { expr = true })
-      vim.keymap.set("i", "<c-x>", function()
-        return vim.fn["codeium#Clear"]()
-      end, { expr = true })
-    end,
   },
   {
     "olexsmir/gopher.nvim",
