74dfb16fe327685123d7c386ea2c42a65d7aade5
Daniel Wiratman <danielwiratman@gmail.com>
2023-05-20 22:17:25 +0700

    Added pomodoro timer

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -79,6 +79,18 @@ local plugins = {
 			"nvim-telescope/telescope.nvim",
 		},
 	},
+	{
+		"wthollingsworth/pomodoro.nvim",
+		dependencies = "MunifTanjim/nui.nvim",
+		config = function()
+			require("pomodoro").setup({
+				time_work = 25,
+				time_break_short = 5,
+				time_break_long = 20,
+				timers_to_long_break = 4,
+			})
+		end,
+	},
 	-- To make a plugin not be loaded
 	-- {
 	--   "NvChad/nvim-colorizer.lua",
